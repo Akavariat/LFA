e63f328721a6b7a9f8794ecb20a8690d4f0d0640
Rodrigo <prehistoriclife65@gmail.com>
2020-03-02 09:44:37 -0300

    Update Moeda.py

--- Moeda.py
+++ Moeda.py
@@ -1,24 +1,24 @@
 def read_coin():
-  x = int(input("Digite o valor da moeda: "))
+  x = float(input("Digite o valor da moeda: "))
 
   return x
 
 def check_coin():
   total = 0
-  allowed = [10, 25]
+  allowed = [0.10, 0.25]
 
-  while(total <= 45):
+  while(total <= 0.45):
     coin = read_coin()
 
     if coin not in allowed:
       print("Moeda inválida!")
       return 0
 
-    if total == 45:
+    if total == 0.45:
       return 1
 
-    if total >= 45:
-        print("Toma sua coca! Perdeu {} centavos!".format(total - 45))
+    if total >= 0.45:
+        print("Toma sua coca! Perdeu {0:.0f} centavos!".format((total - 0.45) * 100))
     
 
 res = check_coin()
